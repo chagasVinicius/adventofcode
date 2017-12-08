f960d8d47a1ff34aa6f6b2e8950bf8d1088de246
chagasVinicius <chagas.vis@gmail.com>
2017-12-08 20:03:07 -0200

    try 2

--- day01/function_01.hs
+++ day01/function_01.hs
@@ -1,6 +1,13 @@
 import System.IO
+import Data.Char
+
+sumRepeated :: String -> Integer
+sumRepeated (x:xs) = let a = digitToInt x
+                     print a
+
 
 main = do
   content <- readFile "/home/chagas/Documents/haskell-try/advetofcode/day01/input.txt"
-  let  c = head content
-  print c
+  let c = head content
+      x = digitToInt c
+  print x 
