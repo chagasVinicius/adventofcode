7be2a60cab032465e1d6072a8a9a1b35ee3790d9
chagasVinicius <chagas.vis@gmail.com>
2017-12-13 13:51:09 -0200

    first part done

--- day01/function_01.hs
+++ day01/function_01.hs
@@ -1,25 +1,21 @@
 import System.IO
 import Data.Char
 
--- sumRepeated :: String -> [Int]
--- sumRepeated [] = []
--- sumRepeated (x:xs) = [digitToInt x]
-
 readString :: String -> [Int]
 readString [] = []
-readString (x:xs) = [digitToInt x] ++ readString xs
+readString (x:xs) 
+  | x == '\n' = readString xs
+  | otherwise = [digitToInt x] ++ readString xs
 
-sumRepeated :: String -> String
-sumRepeated [] = []
-sumRepeated [x] = []
+sumRepeated :: [Int] -> Int
 sumRepeated (x:z:zs)
-  | x == z = [x] ++ sumRepeated (zs)
-  | otherwise = sumRepeated (x:zs)
+  | x == z = x + sumRepeated (z:zs)
+  | otherwise = sumRepeated (z:zs)
+sumRepeated _ = 0
 
 main = do
   content <- readFile "/home/chagas/Documents/haskell-try/advetofcode/day01/input.txt"
-  let s = sumRepeated content
-      l = readString s
-      final = sum l
+  let l = readString content
+      final = sumRepeated (l ++ (take 1 l))
 
   print final
