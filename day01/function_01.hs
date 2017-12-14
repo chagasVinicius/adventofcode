762290ecc9f693c8109c3cce00b39ead6cbeb695
chagasVinicius <chagas.vis@gmail.com>
2017-12-14 13:14:46 -0200

    Part 2 done!

--- day01/function_01.hs
+++ day01/function_01.hs
@@ -13,9 +13,18 @@ sumRepeated (x:z:zs)
   | otherwise = sumRepeated (z:zs)
 sumRepeated _ = 0
 
+sumRepeated' :: Int -> [Int] -> [Int] -> Int
+sumRepeated' acc [] [] = acc
+sumRepeated' acc (x:xs) (y:ys)
+  | x == y = sumRepeated' (acc + (x + y)) xs ys
+  | otherwise = sumRepeated' acc xs ys
+
 main = do
   content <- readFile "/home/chagas/Documents/haskell-try/advetofcode/day01/input.txt"
   let l = readString content
-      final = sumRepeated (l ++ (take 1 l))
+      final1 = sumRepeated (l ++ (take 1 l))
+      split = div (length l) 2
+      (l0, l1) = splitAt split l
+      final2 = sumRepeated' 0 l0 l1
 
-  print final
+  print (final1, final2)
