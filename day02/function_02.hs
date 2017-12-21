15d7d29f6a08aba6db84e4e2d2c186cd371ee518
chagasVinicius <chagas.vis@gmail.com>
2017-12-21 13:22:13 -0200

    Day 02 Done

--- day02/function_02.hs
+++ day02/function_02.hs
@@ -5,16 +5,26 @@ module Main (main) where
 import Data.Char
 import System.IO
 
--- usar reads :: [(Integer, String)]
--- usar words
-
-checksum :: Int -> Int -> [Integer] -> Int
+checksum :: Int -> Int -> [Int] -> Int
 checksum max min [] = max - min
-checksum max min (x0:x1:xs)
-  | 
+checksum max min (x0:xs)
+  | x0 >= max = checksum x0 min xs
+  | x0 < min = checksum max x0 xs
+  | otherwise = checksum max min xs
 
-slistToInt :: [String] -> [Integer]
-slistToInt (xs) = map (read :: String -> Integer) xs
+checksum' :: Int -> Int -> [Int] -> Int
+checksum' _ res [] = res
+checksum' div_ res (x0:xs)
+  | mod div_ x0 == 0 = checksum' div_ (div div_ x0) xs
+  | mod x0 div_ == 0 = checksum' div_ (div x0 div_) xs
+  | otherwise = checksum' div_ res xs
+
+totalSum :: [Int] -> [Int]
+totalSum [] = []
+totalSum (x:xs) = [checksum' x 0 xs] ++ totalSum xs
+
+slistToInt :: [String] -> [Int]
+slistToInt (xs) = map (read :: String -> Int) xs
 
 getLines :: IO [String]
 getLines = isEOF >>= \eof ->
@@ -29,4 +39,5 @@ main = do
   input <- getLines
   let sinput = map words input
       iinput = map slistToInt sinput
-  print $ iinput
+  print $ sum (map (\(x0:xs) -> checksum x0 x0 xs) iinput)
+  print $ sum (map sum (map totalSum iinput))
